ad89aeb651e066b8aad2f4040ff9e73f534d747a
Raoul Scholtes <s8rlscho@stud.uni-saarland.de>
2020-07-07 23:36:46 +0200

    Improved Output

--- core/methods/select.py
+++ core/methods/select.py
@@ -23,13 +23,20 @@ from core.colors import color
 select specific payloads or nullbytes for phase 2
 @payloadlist: payloads or nullbytes found in phase 1
 """
-def select(payloadlist):
+def select(payloadlist, nullbytes=False):
     #filter duplicates
     payloadlist = list(set(payloadlist))
-    print(color.BOLD+"\n  Operative Payloads:\n"+color.END+" "+color.O+str(payloadlist)+color.END)
+    if nullbytes:
+        print("\n{0}[+]{1} Operative nullbytes ({2}{3}{1}):\n {4}".format(color.RD, color.END, color.BOLD, len(payloadlist), str(payloadlist)))
+    else:
+        print("\n{0}[+]{1} Operative payloads ({2}{3}{1}):\n {4}".format(color.RD, color.END, color.BOLD, len(payloadlist), str(payloadlist)))
+    #print(color.BOLD+"\n  Operative Payloads:\n"+color.END+" "+color.O+str(payloadlist)+color.END)
     invalid = True
     while invalid:
-        payloads = input("Select payloads indexes for the attack (comma-separated) :> ")
+        if nullbytes:
+            payloads = input("Select nullbyte indexes for the attack (comma-separated) :> ")
+        else:
+            payloads = input("Select payload indexes for the attack (comma-separated) :> ")
         try:
             selected = [payloadlist[int(i.strip())] for i in payloads.split(",")]
             invalid = False
