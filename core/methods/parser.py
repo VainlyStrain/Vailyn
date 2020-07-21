b18f63b5e709d059355db797cd8702e45d4b2804
Raoul Scholtes <s8rlscho@stud.uni-saarland.de>
2020-07-21 16:18:30 +0200

    grouped depth and checkdepth arguments together & renamed cookie argument

--- core/methods/parser.py
+++ core/methods/parser.py
@@ -35,13 +35,11 @@ mandatory:
 additional:
   -p PAM, --param PAM   {0}query parameter to use for --attack 1{1}
   -s, --all             {0}display every path tried, even 404s{1}
-  -d INT, --depth INT   {0}max. depth of checking and dir permutation level{1}
+  -d I J, --depths I J  {0}depths of checking (I: phase 1, J: phase 2){1}
   -n, --loot            {0}Download found files into the loot folder{1}
-  -c INT, --checkdepth INT
-                        {0}Max. Depth of checking for phase 1{1}
+  -c FIL, --cookie FIL  {0}File containing authentication cookie (if needed){1}
   -h, --help            {0}show this help menu and exit{1}
   -i FIL, --check FIL   {0}File to check for in Phase 1 (df: /etc/passwd){1}
-  -t FIL, --cookie FIL  {0}File containing authentication cookie (if needed){1}
   -q VIC2, --vic2 VIC2  {0}Attack Target, part 2 (post injection point){1}'''.format(color.RC, color.END))
 
 class VainFormatter(argparse.RawDescriptionHelpFormatter):
@@ -49,50 +47,47 @@ class VainFormatter(argparse.RawDescriptionHelpFormatter):
         if prefix is None:
             prefix = color.RC + 'Vsynta ' + color.END
             #return super(VainFormatter, self).add_usage("{}Vailyn{} [-v VIC] [-a ACK] [-p PARAM] [-s]\n          [-l FIL PATH] [-d INT] [--loot]\n        [-f] [-h] [--vic2 VIC2]".format(color.RB,color.END), actions, groups, prefix)
-            return super(VainFormatter, self).add_usage("{}Vailyn{} -v VIC -a ACK -l FIL PATH \n        [-p PAM] [-s] [-d INT] [-n] \n      [-c INT] [-i FIL]\n    [-q VIC2] [-h]".format(color.RB,color.END), actions, groups, prefix)
+            return super(VainFormatter, self).add_usage("{}Vailyn{} -v VIC -a ACK -l FIL PATH \n        [-p PAM] [-s] [-d I J] [-n] \n      [-c FIL] [-i FIL]\n    [-q VIC2] [-h]".format(color.RB,color.END), actions, groups, prefix)
 
 def build_parser():
     p = ArgumentParser(formatter_class=VainFormatter,add_help=False)
     p.add_argument('-v', '--victim',
-                   help="set attack target address [scheme:// needed]",
+                   help="Target to attack, part 1 [pre injection point]",
                    metavar="VIC")
     p.add_argument('-a', '--attack',
-                   help="attack [1]:query [2]:inpath",
+                   help="Attack type (int)[1: query, 2: path, 3:cookie]",
                    metavar="ACK",
                    type=int)
     p.add_argument('-s', '--all',
-                   help="Show 404s?",
+                   help="display every path tried, even 404s",
                    action="store_true",)
-    p.add_argument('-d', '--depth',
-                   help="1 › max. nr of ../ and dir permutation level",
-                   metavar="INT",
-                   type=int)
+    p.add_argument('-d', '--depths',
+                   help="depths of checking (I: phase 1, J: phase 2)",
+                   metavar="I J",
+                   type=int,
+                   nargs=2)
     p.add_argument('-h', '--help',
                    help="0 » display this help message and exit",
                    action="help",
                    default=argparse.SUPPRESS)
     p.add_argument('-p', '--param',
-                   help="A › Query parameter for file",
-                   metavar="PARAM")
+                   help="query parameter to use for --attack 1",
+                   metavar="PAM")
     p.add_argument('-l', '--lists',
-                   help="1 » Dictionaries",
+                   help="Dictionaries to use (see templates for syntax)",
                    nargs=2,
                    metavar=("FIL","PATH"))
     p.add_argument('-n', '--loot',
-                   help="1 › Download found files into loot",
+                   help="Download found files into the loot folder",
                    action="store_true")
     p.add_argument('-q', '--vic2',
-                   help="A › Attack target part 2",
+                   help="Attack Target, part 2 (post injection point)",
                    metavar=("VIC2"))
     p.add_argument('-i', '--check',
-                   help="A › Attack target part 2",
-                   metavar=("FILE"))
-    p.add_argument('-t', '--cookie',
-                   help="A › Attack target part 2",
-                   metavar=("FILE"))
-    p.add_argument('-c', '--checkdepth',
-                   help="A › Attack target part 2",
-                   metavar=("INT"),
-                   type=int)
+                   help="File to check for in Phase 1 (df: /etc/passwd)",
+                   metavar=("FIL"))
+    p.add_argument('-c', '--cookie',
+                   help="File containing authentication cookie (if needed)",
+                   metavar=("FIL"))
                
     return p 
