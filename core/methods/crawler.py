95b60966347405a8e611df17192b1eeedd3d3d24
Raoul Scholtes <s8rlscho@stud.uni-saarland.de>
2020-08-09 01:29:27 +0200

    crawler fixes

--- core/methods/crawler.py
+++ core/methods/crawler.py
@@ -19,7 +19,7 @@ _____, ___
 
 
 import scrapy
-import json, os
+import json, os, time
 import subprocess
 
 from scrapy import Request
@@ -27,6 +27,7 @@ from scrapy.linkextractors import LinkExtractor
 from multiprocessing.pool import ThreadPool as Pool
 
 from core.variables import viclist, processes, stable, cachedir
+from core.colors import color
 from core.methods.attack import phase1
 from core.methods.cache import parseUrl
 from core.methods.cookie import getCookie
@@ -45,7 +46,8 @@ class UrlSpider(scrapy.Spider):
     def parse(self, response):
         le = LinkExtractor(allow=".*{}.*".format(domain)) 
         for link in le.extract_links(response):
-            viclist.append(link.url)
+            if link.url not in viclist:
+                viclist.append(link.url)
             yield Request(link.url, callback=self.parse)
 
 class FormSpider(scrapy.Spider):
@@ -56,15 +58,22 @@ class FormSpider(scrapy.Spider):
         return
 
 def arjunEnum():
-    with open("core/tmp/in.txt", "w") as vicfile:
+    subdir = parseUrl(viclist[0])
+    if not os.path.exists(cachedir+subdir):
+        os.makedirs(cachedir+subdir)
+    with open(cachedir+subdir+"spider-phase0.txt", "w") as vicfile:
         for target in viclist:
             vicfile.write(target + "\n")
     if stable:
-        subprocess.run(["python3", "lib/Arjun/arjun.py", "-o", "core/tmp/out.json", "--urls", "core/tmp/in.txt", "--get"])
+        subprocess.run(["python3", "lib/Arjun/arjun.py", "-o", cachedir+subdir+"spider-phase1.json", "--urls", cachedir+subdir+"spider-phase0.txt", "--get", "-f", "lib/Arjun/db/params.txt"])
     else:
-        subprocess.run(["python3", "lib/Arjun/arjun.py", "-t", str(processes), "-o", "core/tmp/out.json", "--urls", "core/tmp/in.txt", "--get"])
+        subprocess.run(["python3", "lib/Arjun/arjun.py", "-t", str(processes), "-o", cachedir+subdir+"spider-phase1.json", "--urls", cachedir+subdir+"spider-phase0.txt", "--get", "-f", "lib/Arjun/db/params.txt"])
     
-    siteparams = json.load("core/tmp/out.json")
+
+    siteparams = None
+    with open(cachedir+subdir+"spider-phase1.json") as f:
+        siteparams = json.load(f)
+    assert siteparams != None
     return siteparams
 
 def analyzeParam(siteparams, paysplit, victim2, verbose, depth, file, authcookie):
@@ -72,9 +81,13 @@ def analyzeParam(siteparams, paysplit, victim2, verbose, depth, file, authcookie
     subdir = parseUrl(viclist[0])
     for victim, paramlist in siteparams.items():
         sub = {}
+        print("\n{0}[INFO]{1} param{4}|{2} Attacking {3}".format(color.RD, color.END + color.O, color.END, victim, color.END+color.RD))
+        time.sleep(1.5)
         for param in paramlist:
             payloads = []
             nullbytes = []
+            print("\n{0}[INFO]{1} param{4}|{2} Using {3}\n".format(color.RD, color.END + color.O, color.END, param, color.END+color.RD))
+            time.sleep(1.5)
             with Pool(processes=processes) as pool:
                 res = [pool.apply_async(phase1, args=(1,victim,victim2,param,None,"",verbose,depth,l,file,authcookie,"",)) for l in paysplit]
                 for i in res:
@@ -83,10 +96,12 @@ def analyzeParam(siteparams, paysplit, victim2, verbose, depth, file, authcookie
                     payloads += tuples[0]
                     nullbytes += tuples[1]
             sub[param] = (payloads, nullbytes)
+            time.sleep(3)
         result[victim] = sub
     if not os.path.exists(cachedir+subdir):
         os.makedirs(cachedir+subdir)
-    json.dump(result, cachedir+subdir+"spider-phase1.json", sort_keys=True, indent=4)
+    with open(cachedir+subdir+"spider-phase2.json", "w+") as f:
+        json.dump(result, f, sort_keys=True, indent=4)
     return result
 
 def analyzePath(paysplit, victim2, verbose, depth, file, authcookie):
@@ -95,6 +110,8 @@ def analyzePath(paysplit, victim2, verbose, depth, file, authcookie):
     for victim in viclist:
         payloads = []
         nullbytes = []
+        print("\n{0}[INFO]{1} path{4}|{2} Attacking {3}\n".format(color.RD, color.END + color.O, color.END, victim, color.END+color.RD))
+        time.sleep(1.5)
         with Pool(processes=processes) as pool:
             res = [pool.apply_async(phase1, args=(2,victim,victim2,"",None,"",verbose,depth,l,file,authcookie,"",)) for l in paysplit]
             for i in res:
@@ -103,9 +120,12 @@ def analyzePath(paysplit, victim2, verbose, depth, file, authcookie):
                 payloads += tuples[0]
                 nullbytes += tuples[1]
         result[victim] = (payloads, nullbytes)
+        time.sleep(3)
     if not os.path.exists(cachedir+subdir):
         os.makedirs(cachedir+subdir)
-    json.dump(result, cachedir+subdir+"spider-phase2.json", sort_keys=True, indent=4)
+
+    with open(cachedir+subdir+"spider-phase3.json", "w+") as f:
+        json.dump(result, f, sort_keys=True, indent=4)
     return result
 
 def analyzeCookie(paysplit, victim2, verbose, depth, file, authcookie):
@@ -114,9 +134,16 @@ def analyzeCookie(paysplit, victim2, verbose, depth, file, authcookie):
     for victim in viclist:
         sub = {}
         cookie = getCookie(victim)
+        if len(cookie.keys()) < 1:
+            print("\n{0}[INFO]{1} cookie{4}|{2} No cookies available for {3}.\n".format(color.RD, color.END + color.O, color.END, victim, color.END+color.RD))
+            continue
+        print("\n{0}[INFO]{1} cookie{4}|{2} Attacking {3}\n".format(color.RD, color.END + color.O, color.END, victim, color.END+color.RD))
+        time.sleep(1.5)
         for key in cookie.keys():
             payloads = []
             nullbytes = []
+            print("\n{0}[INFO]{1} cookie{4}|{2} Using {3}\n".format(color.RD, color.END + color.O, color.END, key, color.END+color.RD))
+            time.sleep(1.5)
             with Pool(processes=processes) as pool:
                 res = [pool.apply_async(phase1, args=(3,victim,victim2,"",cookie,key,verbose,depth,l,file,authcookie,"",)) for l in paysplit]
                 for i in res:
@@ -128,5 +155,6 @@ def analyzeCookie(paysplit, victim2, verbose, depth, file, authcookie):
         result[victim] = sub
     if not os.path.exists(cachedir+subdir):
         os.makedirs(cachedir+subdir)
-    json.dump(result, cachedir+subdir+"spider-phase2.json", sort_keys=True, indent=4)
+    with open(cachedir+subdir+"spider-phase4.json", "w+") as f:
+        json.dump(result, f, sort_keys=True, indent=4)
     return result
