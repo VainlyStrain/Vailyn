0104d5bd6b7476218820f81fa434347017499e85
Raoul Scholtes <s8rlscho@stud.uni-saarland.de>
2020-09-02 18:41:06 +0200

    fix version link

--- core/methods/version.py
+++ core/methods/version.py
@@ -24,7 +24,7 @@ from core.variables import e_version
 def checkUpdate():
     try:
         s = session()
-        onver = s.get("https://raw.githubusercontent.com/VainlyStrain/Vailyn/core/doc/VERSION", timeout=5).text.strip()
+        onver = s.get("https://raw.githubusercontent.com/VainlyStrain/Vailyn/master/core/doc/VERSION", timeout=5).text.strip()
         localmain = e_version.split("-")[0]
         localrev = e_version.split("-")[1]
         locallist = localmain.split(".")
