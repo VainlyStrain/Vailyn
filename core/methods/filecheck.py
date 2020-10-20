6efbe92a7907e38a304093697c72da8ce41c120c
Raoul Scholtes <s8rlscho@stud.uni-saarland.de>
2020-10-21 00:53:04 +0200

    improved false positive prevention

--- core/methods/filecheck.py
+++ core/methods/filecheck.py
@@ -29,13 +29,28 @@ This method filters out false positives. It does so by many different checks:
 def filecheck(r, con2, con3, payload, post=False):
     con = r.content
     conn = str(con).lower()
+
+    try:
+        #false positive prevention in case server has default include which disappears in case of file
+        #not found
+        conb2 = con2.decode().lower().strip()
+        conb = con.decode().lower().strip()
+        check2 = conb not in conb2
+    except:
+        """
+        if we get an encoding error, it may mean 2 things:
+          a) we just successfully included a binary file --> vulnerable
+          b) it happened all the time, so return true to prevent false negatives
+        """
+        check2 = True
     #prevents Vailyn to be stuck with long binary files (like zip archives)
     if r.encoding == None:
         r.encoding = "utf-8"
     txt = r.text.lower()
     check = (con != con2 and "[<a href='function.main'>function.main</a>" not in conn and "[<a href='function.include'>function.include</a>" not in conn 
     and ("failed opening" not in conn and "for inclusion" not in conn) and "failed to open stream:" not in conn and "open_basedir restriction in effect" not in conn 
-    and payload.lower() not in txt and unquote(payload).lower() not in txt and "file_exists() expects parameter 1 to be a valid path" not in conn and conn != "b''")
+    and payload.lower() not in txt and unquote(payload).lower() not in txt and "file_exists() expects parameter 1 to be a valid path" not in conn and conn != "b''"
+    and check2)
     if con3:
         check = check and con3 != con
     return check
