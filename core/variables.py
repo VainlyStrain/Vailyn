02bb918c14d32ba425a9709a79d22ebe8a0c536e
Raoul Scholtes <s8rlscho@stud.uni-saarland.de>
2020-10-23 00:37:01 +0200

    minor changes

--- core/variables.py
+++ core/variables.py
@@ -79,10 +79,11 @@ else:
 
 verbose = False
 
-version = "2.5"
+version = ""
 e_version = ""
 with open("core/doc/VERSION", "r") as versionfile:
     e_version = versionfile.read().strip()
+version = ".".join(e_version.split(".")[0:-1])
 
 user_agents = [
     "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36",
